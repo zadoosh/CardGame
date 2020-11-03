92d87e40260f22fdb52a39679551ca8d70e8d1aa
zadoosh <zadoosh25@gmail.com>
2020-11-03 17:40:53 -0500

    another test

--- Chain.h
+++ Chain.h
@@ -9,6 +9,7 @@ class Chain {
         Chain(istream& in, const CardFactory* cf); //is a constructor which accepts an istream and reconstructs the chain from file when a game is resumed. 
         Chain<T>& operator+=(Card* c); //adds a card to the Chain. If the run-time type does not match the template type of the chain and exception of type IllegalType needs to be raised.
         int sell(); //counts the number cards in the current chain and returns the number coins according to the function Card::getCardsPerCoin. 
+    private:
 };
 
 
--- DiscardPile.h
+++ DiscardPile.h
@@ -1,2 +1,13 @@
 #include <iostream>
+#include "Card.h"
+#include "CardFactory.h"
 using namespace std;
+
+class Hand {
+    public:
+        Hand(istream& in, const CardFactory* cf);
+        Hand& operator+=(Card*);
+        Card* play();
+        Card* top();
+        Card* operator[](int);
+};
