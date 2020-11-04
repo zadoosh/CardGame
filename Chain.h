7486d82d47459f5b81b997a420a7119bb55208bc
zadoosh <zadoosh25@gmail.com>
2020-11-04 11:23:22 -0500

    added a default constructor

--- Chain.h
+++ Chain.h
@@ -7,7 +7,7 @@ using namespace std;
 
 class Chain {
     public:
-        Chain(); // default constructor
+        Chain(Card* firstCard); // default constructor
         Chain(istream& in, const CardFactory* cf); //is a constructor which accepts an istream and reconstructs the chain from file when a game is resumed. 
         Chain<T>& operator+=(Card* c); //adds a card to the Chain. If the run-time type does not match the template type of the chain and exception of type IllegalType needs to be raised.
         int sell(); //counts the number cards in the current chain and returns the number coins according to the function Card::getCardsPerCoin. 
@@ -15,9 +15,9 @@ class Chain {
         vector<Card> cardChain;
 };
 
-Chain::Chain() //default constructor 
+Chain::Chain(Card* firstCard) //default constructor 
 {
-
+    cardChain.push_back(*firstCard);
 }
 
 Chain::Chain(istream& in, const CardFactory* cf) //constructor for when the game is resumed from file
